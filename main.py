fbb055f4e8232a6fc2e3ecc74f63b3c4960e08c3
Winterson <u19cse1028@cit.ac.in>
2025-12-15 01:57:27 +0530

    add: main file

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from src.file_converter import (
+    convert_doc_with_image_annotation,
+    export_to_md_with_image_ref,
+)
+
+
 def main():
-    print("Hello from doc-processing!")
+    result = convert_doc_with_image_annotation("docs/test-document-1.pdf")
+    print(
+        result.document.export_to_markdown(
+            mark_annotations=True, include_annotations=True
+        )
+    )
+    export_to_md_with_image_ref(result, "cs")
 
 
 if __name__ == "__main__":
